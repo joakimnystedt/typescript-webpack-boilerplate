7c2688e88fa90a47ceefb58d3850b0eb12a147f1
Joakim Nystedt <joakim@sjutton5.se>
2022-02-04 16:34:11 +0100

    three-shaking

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,9 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
     mode: 'development',
     entry: './src/index.ts',
-    //devtool: 'inline-source-map',
+    optimization: {
+        usedExports: true,
+    },
     devServer: {
         static: './dist',
     },
