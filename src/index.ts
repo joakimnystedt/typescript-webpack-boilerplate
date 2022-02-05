ef4e8759827e61b1c0c7d930328fae6b469ae33c
Joakim Nystedt <joakim@sjutton5.se>
2022-02-05 10:22:08 +0100

    ts settings

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import './assets/styles.scss'
 import { getPersons, getMergedAges } from './services/exampleService'
 
-window.addEventListener('DOMContentLoaded', (event) => {
+window.addEventListener('DOMContentLoaded', () => {
     console.log('DOM fully loaded and parsed')
     console.log(getPersons, 'All persons in an array')
     console.log(getMergedAges, 'All ages merged!')
